6574f701ee3ade67c56b5fa4f5a12f9e2c6909dd
Jinyan Jiang <jinyanj@berkeley.edu>
2018-02-15 20:46:47 -0800

    ECDF

--- ECDF.py
+++ ECDF.py
@@ -11,3 +11,20 @@ def ecdf(data):
     y = np.arange(1, n+1) / n
 
     return x, y
+
+
+
+x y = ecdf(data)
+
+# Generate plot
+_ = plt.plot(x, y, marker='.', linestyle='none')
+
+# Make the margins nice
+plt.margins(0.02)
+
+# Label the axes
+_ = plt.xlabel('xlabel')
+_ = plt.ylabel('ylabel')
+
+# Display the plot
+plt.show()
